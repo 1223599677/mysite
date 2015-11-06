35250a92b9f510671ff6a5ea7a04bdbaec6262b9
Weizhong Tu <mail@tuweizhong.com>
2015-11-06 21:54:37 +0800

    private folder

--- utils/models.py
+++ utils/models.py
@@ -44,7 +44,7 @@ class PrivateGenome(TimeModel):
 
     def get_browse_url(self):
         url = ('http://genebrowser.lifemodules.org/jbrowse/?data='
-               'data/{username}/{name}_{random_string}'
+               'data/private/{username}/{name}_{random_string}'
              ).format(
                 username = self.owner.username,
                 name = self.get_name(),
